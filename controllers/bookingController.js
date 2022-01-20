663f787d7efef50422bb1264ea87aa37dd37bf2d
MOHAN RAJ <mohanraj.2k00@gmail.com>
2022-01-20 20:40:22 +0530

    debugging

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,12 +60,14 @@ exports.getCheckoutSession = catchAsync(
 //   }
 // );
 const createBookingCheckout = async (session) => {
+  console.log(session);
   const tour = session.client_reference_id;
   const user = (
     await User.findOne({ email: session.customer_email })
   ).id;
   const price =
     session.line_item_group.line_items[0].total / 100;
+  console.log(tour, user, price);
   await Booking.create({ tour, user, price });
 };
 
